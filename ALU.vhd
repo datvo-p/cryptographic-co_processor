5142141e0e1e732fdb32c8c499d3f42f074fc0a2
Dat Vo <datvo.p@gmail.com>
2020-07-27 19:59:53 -0400

    updated ALU2

--- ALU.vhd
+++ ALU.vhd
@@ -11,31 +11,29 @@ port(
 A: in STD_LOGIC_VECTOR(15 downto 0);
 B: in STD_LOGIC_VECTOR(15 downto 0);
 Sel: in STD_LOGIC_VECTOR(3 downto 0);
-result: out STD_LOGIC_VECTOR(15 downto 0)
+result: inout STD_LOGIC_VECTOR(15 downto 0)
 );
 end ALU;
 
 
 
 architecture Behavioral of ALU is
-signal AAAA,BBBB:STD_logic_vector(15 downto 0);
+signal AAAA,BBBB:STD_logic_vector(16 downto 0);
+signal selsig: STD_logic_vector(16 downto 0);
 begin
 
 AAAA<='0'&A(15 downto 0);
 BBBB<='0'&B(15 downto 0);
+selsig<='0'&result(15 downto 0);
 
-    with Sel select result <=
+    with Sel select selsig <=
         AAAA+BBBB when  "0000",
         AAAA-BBBB when "0001",
        (AAAA and BBBB) when "0010",
         AAAA or BBBB when "0011",
         AAAA xor BBBB when "0100",
         not(AAAA) when "0101",
-        AAAA when "0110";
-        
-        
-        
-        
-        
-        
+        AAAA when "0110",
+        "00000000000000000" when others;
+
 end Behavioral;
--- Multilexer.vhd
+++ Multilexer.vhd
@@ -7,7 +7,7 @@ entity Multilexer is
  port(
  A : in STD_LOGIC_VECTOR(15 downto 0); -----ALU input
  B: in STD_LOGIC_VECTOR(15 downto 0); -------shifter input
- C: in STD_LOGIC_VECTOR(7 downto 0); ------non linear look up input
+ C: in STD_LOGIC_VECTOR(15 downto 0); ------non linear look up input
  S : in STD_LOGIC_VECTOR(3 downto 0);--------control logic
  F : out STD_LOGIC_vector(15 downto 0)
  );
@@ -18,16 +18,16 @@ architecture Behavioral of Multilexer is
 
 begin
        F  <=A(15 downto 0) when S = "0000" else
-            A(15 downto 0)  when S = "0001" else
+            A(15 downto 0) when S = "0001" else
            A(15 downto 0)  when S = "0010" else
             A(15 downto 0) when S = "0011" else
-            A(15 downto 0)  when S = "0100" else
-            A(15 downto 0)  when S = "0101" else
-            A(15 downto 0)  when S = "0110" else
-            A(15 downto 0)  when S = "0111" else
+            A(15 downto 0) when S = "0100" else
+            A(15 downto 0) when S = "0101" else
+            A(15 downto 0) when S = "0110" else
+            A(15 downto 0) when S = "0111" else
             B(15 downto 0) when S = "1000" else
             B(15 downto 0) when S = "1001" else
             B(15 downto 0) when S ="1010" else
-            C(15 downto 0);
-
+            C(15 downto 0) when S="1011" else 
+            "0000000000000000";
 end Behavioral;
